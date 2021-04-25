638bf1b5f56e66f88e1c37c36204871f489ea485
Iurii Bairamukov <juribai@mail.ru>
2021-04-25 22:32:13 +0700

    feat: создать классы для Player и Game
    перенести логику из модулей в классы

--- main.js
+++ main.js
@@ -1,54 +1,29 @@
-import { enemyAttack, playerAttack } from './modules/battle.js';
 import { generateLogs } from './modules/logs.js';
 import { getHP } from './modules/getHP.js';
-import { player1, player2 } from './modules/player.js';
+import { player1, player2 } from './modules/players.js';
 import { createElement } from './modules/utils.js';
-import { showResult } from './modules/result.js';
+import { Game } from './modules/game.js';
+import { ATTACK, HIT } from './modules/constants.js';
+import { randomValue } from './modules/utils.js';
 
 const arenas = document.querySelector('.arenas');
 const formFight = document.querySelector('.control');
 const formFightButton = formFight.querySelector('.button');
 const chat = document.querySelector('.chat');
 
-const createPlayer = (playerConfig) => {
-  const { player, hp, name, img } = playerConfig;
-  const newPlayer = createElement('div', `player${player}`);
+const game = new Game({
+  generateLogs,
+  formFight,
+  HIT,
+  ATTACK,
+  randomValue,
+  getHP,
+  chat,
+  player1,
+  player2,
+  arenas,
+  formFightButton,
+  createElement,
+});
 
-  const progressbar = createElement('div', 'progressbar');
-  const character = createElement('div', 'character');
-
-  const life = createElement('div', 'life');
-  life.style.width = `${hp}%`;
-
-  const namePlayer = createElement('div', 'name');
-  namePlayer.innerText = name;
-
-  const image = createElement('img');
-  image.src = img;
-
-  newPlayer.appendChild(progressbar);
-  newPlayer.appendChild(character);
-
-  progressbar.appendChild(life);
-  progressbar.appendChild(namePlayer);
-
-  character.appendChild(image);
-
-  return newPlayer;
-};
-
-arenas.appendChild(createPlayer(player1));
-arenas.appendChild(createPlayer(player2));
-
-generateLogs(chat, 'start', player2, player1);
-
-const submitFight = (e) => {
-  e.preventDefault();
-  const enemy = enemyAttack();
-  const player = playerAttack(formFight);
-  getHP(chat, player1, player2, player, enemy);
-  getHP(chat, player2, player1, enemy, player);
-  showResult(formFightButton, arenas, chat, player1, player2);
-};
-
-formFight.addEventListener('submit', (e) => submitFight(e));
+game.start();
---
+++ modules/players.js
@@ -0,0 +1,17 @@
+import { Player } from './player.js';
+
+const player1 = new Player({
+  player: 1,
+  name: 'Liukang',
+  hp: 100,
+  img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+});
+
+const player2 = new Player({
+  player: 2,
+  name: 'Sonya',
+  hp: 100,
+  img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+});
+
+export { player1, player2 };
